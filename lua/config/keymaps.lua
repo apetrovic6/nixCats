580efc7e7abcfdaab33f2cfe46261cbde9c78bac
apetrovic6 <petrovicante6@gmail.com>
2025-02-10 17:39:49 +0100

    Add keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,5 @@ vim.keymap.set('n', '<C-a>', '<C-w><C-h>', { noremap = true, desc = 'Move focus 
 vim.keymap.set('n', '<C-h>', '<C-w><C-l>', { noremap = true, desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-e>', '<C-w><C-j>', { noremap = true, desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-i>', '<C-w><C-k>', { noremap = true, desc = 'Move focus to the upper window' })
+
+require('which-key').add { '<leader>m', desc = 'Misc' }
