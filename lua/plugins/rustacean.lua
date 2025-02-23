ab0453943d507a3b0dfe6738d8ca55f139f6ad81
apetrovic6 <petrovicante6@gmail.com>
2025-02-23 11:06:35 +0100

    update keymap

--- lua/plugins/rustacean.lua
+++ lua/plugins/rustacean.lua
@@ -4,7 +4,7 @@ return {
   lazy = false, -- This plugin is already lazy
   config = function()
     local bufnr = vim.api.nvim_get_current_buf()
-    vim.keymap.set('n', '<leader>a', function()
+    vim.keymap.set('n', '<leader>ca', function()
       vim.cmd.RustLsp 'codeAction' -- supports rust-analyzer's grouping
       -- or vim.lsp.buf.codeAction() if you don't want grouping.
     end, { silent = true, buffer = bufnr })
