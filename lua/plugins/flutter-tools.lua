96955bebbd21ae9fe5d73cacb11a63ff80821d05
apetrovic <petrovicante6@gmail.com>
2025-07-20 19:08:25 +0200

    flake update

--- lua/plugins/flutter-tools.lua
+++ lua/plugins/flutter-tools.lua
@@ -2,7 +2,39 @@ return {
   -- 🔌 1. turn off the Mason bridge so it never loads
   { 'jay-babu/mason-nvim-dap.nvim', enabled = false },
 
-  -- 🔌 2. flutter-tools (unchanged except for run_via_dap = true, optional)
+  {
+    'wa11breaker/flutter-bloc.nvim',
+    dependencies = {
+      'nvimtools/none-ls.nvim', -- Required for code actions
+    },
+    opts = {
+      bloc_type = 'equatable', -- Choose from: 'default', 'equatable', 'freezed'
+      use_sealed_classes = false,
+      enable_code_actions = true,
+    },
+    config = function(_, opts)
+      require('flutter-bloc').setup(opts)
+
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = 'dart',
+        callback = function()
+          -- Create Bloc
+          vim.keymap.set('n', '<Leader>cgb', "<cmd>lua require('flutter-bloc').create_bloc()<cr>", {
+            desc = '[C]reate [F]lutter [B]loc',
+            buffer = true,
+          })
+
+          -- Create Cubit
+          vim.keymap.set('n', '<Leader>cgc', "<cmd>lua require('flutter-bloc').create_cubit()<cr>", {
+            desc = '[C]reate [F]lutter [C]ubit',
+            buffer = true,
+          })
+        end,
+      })
+    end,
+  },
+
+  -- 🔌 2. flutter-tools
   {
     'nvim-flutter/flutter-tools.nvim',
     lazy = false,
@@ -13,21 +45,20 @@ return {
     config = {
       debugger = {
         enabled = true,
-        run_via_dap = true, -- let nvim-dap drive :FlutterRun as well
+        run_via_dap = true,
       },
     },
   },
 
-  -- 🔌 3. manual DAP wiring that speaks *Flutter*, not plain Dart
+  -- 🔌 3. DAP configuration
   {
     'mfussenegger/nvim-dap',
     config = function()
       local dap = require 'dap'
 
-      -- one adapter for both Dart CLI *and* Flutter apps
       dap.adapters.dart = {
         type = 'executable',
-        command = 'flutter', -- <-- key change
+        command = 'flutter',
         args = { 'debug_adapter' },
       }
 
@@ -38,10 +69,8 @@ return {
           request = 'launch',
           program = '${workspaceFolder}/lib/main.dart',
           cwd = '${workspaceFolder}',
-
-          -- everything after this line is forwarded to `flutter run`
-          flutterMode = 'debug', -- tell the adapter it’s Flutter
-          toolArgs = { '-d', 'linux' }, -- choose your device (phone, chrome…)
+          flutterMode = 'debug',
+          toolArgs = { '-d', 'linux' },
         },
       }
     end,
